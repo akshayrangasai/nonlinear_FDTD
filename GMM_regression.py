e270e51a2449dfb5d4e153c9b6941f886a890614
Your Name <akshayrangasai.d@gmail.com>
2015-05-05 11:01:02 +0530

    Added test_train files

--- GMM_regression.py
+++ GMM_regression.py
@@ -7,7 +7,7 @@ dataset = np.vstack(set(map(tuple,np.genfromtxt(FILE, delimiter=','))))
 
 def addNoise(snr):
     signal = dataset[:,-1]
-    print signal
+    #print signal
     signalstd = np.std(signal)
     noisestd = signalstd/np.sqrt(snr)
     noise = np.random.normal(0,noisestd,len(signal))
@@ -17,20 +17,30 @@ def addNoise(snr):
 
 def ensemble(value, noise):
 
-    #mixture = GMM(C = 100,epsilon = 1e-20)
+    #mixture = GMM(C = 100)
     mixture = GMM()
     newdataset = addNoise(noise)
+    temp = np.copy(newdataset[:,-1])
+    newdataset[:,-1] = newdataset[:,3]
+    newdataset[:,3] = temp
+    print newdataset[:,-1], newdataset[:,3]
     for ensemble in range(0,value):
         np.random.shuffle(newdataset)
+        train = np.copy(newdataset[0:-10,:])
+        test =  np.copy(newdataset[-10:-1,:])
         mixture.fit(newdataset[0:-10,0:-2],newdataset[0:-10,-1])
         preds = mixture.predict(newdataset[-10:-1,0:-2])
-        errorabs =  abs(dataset[-10:-1,-1]-preds)
+        test[:,-1] = preds
+        errorabs =  abs(dataset[-10:-1,-1]-preds)/(dataset[-10:-1,-1])
         meanerrorabs = np.mean(errorabs)
         stderrorabs = np.std(errorabs)
+        #print errorabs
         print meanerrorabs, stderrorabs
         #plt.plot(abs(dataset[-10:-1,-1]-preds))
         #plt.ylim(-5e-12,5e-12)
         #plt.scatter(dataset[-10:-1,0],dataset[-10:-1,-1])
         #plt.plot(preds)
         #plt.show()
+        np.savetxt('data/train_%d.csv'%ensemble,train,delimiter=',')
+        np.savetxt('data/test_%d.csv'%ensemble,test,delimiter=',')
 ensemble(5, 10)
