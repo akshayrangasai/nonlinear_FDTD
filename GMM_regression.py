49ac68c602a9f70af3eac12c723e44ea74d5053b
Your Name <akshayrangasai.d@gmail.com>
2015-05-12 01:15:00 +0530

    Added everything to the repo

--- GMM_regression.py
+++ GMM_regression.py
@@ -12,8 +12,9 @@ def addNoise(snr):
     signalstd = np.std(signal)
     noisestd = signalstd/np.sqrt(snr)
     noise = np.random.normal(0,noisestd,len(signal))
-    datasetnoisy = dataset
+    datasetnoisy = np.copy(dataset)
     datasetnoisy[:,-1] = datasetnoisy[:,-1] + noise
+    #np.savetxt('data/datanoisy.csv',datasetnoisy,delimiter=',')
     return datasetnoisy
 
 def ensemble(value, noise):
@@ -22,8 +23,8 @@ def ensemble(value, noise):
     mixture = GMM()
     newdataset = addNoise(noise)
     temp = np.copy(newdataset[:,-1])
-    #newdataset[:,-1] = newdataset[:,2]
-    #newdataset[:,2] = temp
+    newdataset[:,-1] = newdataset[:,2]
+    newdataset[:,2] = temp
     #print newdataset[:,-1], newdataset[:,3]
     for ensemble in range(0,value):
         np.random.shuffle(newdataset)
@@ -32,20 +33,20 @@ def ensemble(value, noise):
         test_pred = np.copy(test)
         mixture.fit(newdataset[0:-10,0:-2],newdataset[0:-10,-1])
         preds = mixture.predict(newdataset[-10:-1,0:-2])
-        test_pred[:,-1] = preds
+        test_pred[:,-2:-1] = preds
         errorabs =  abs(dataset[-10:-1,-1]-preds)/(dataset[-10:-1,-1])
         meanerrorabs = np.mean(errorabs)
         stderrorabs = np.std(errorabs)
-        #print errorabs
-        print meanerrorabs, stderrorabs
+        print preds
+        #print meanerrorabs, stderrorabs
         #plt.plot(abs(dataset[-10:-1,-1]-preds))
         #plt.ylim(-5e-12,5e-12)
         #plt.scatter(dataset[-10:-1,0],dataset[-10:-1,-1])
         #plt.plot(preds)
         #plt.show()
-        np.savetxt('data/forward_train_%d_snr_%d.csv'%(ensemble,noise),train,delimiter=',')
-        np.savetxt('data/forward_test_%d_snr_%d.csv'%(ensemble,noise),test,delimiter=',')
-        np.savetxt('data/forward_test_predict_%d_snr_%d.csv'%(ensemble,noise),test_pred,delimiter=',')
+        np.savetxt('data/new_train_%d_snr_%d.csv'%(ensemble,noise),train,delimiter=',')
+        np.savetxt('data/new_test_%d_snr_%d.csv'%(ensemble,noise),test,delimiter=',')
+        np.savetxt('data/new_test_predict_%d_snr_%d.csv'%(ensemble,noise),test_pred,delimiter=',')
         #sp.io.savemat('data/train_%d_snr_%d.mat'%(ensemble,noise),train)
         #sp.io.savemat('data/test_%d_snr_%d.mat'%(ensemble,noise),test)
         #sp.io.savemat('data/test_predict_%d_snr_%d.mat'%(ensemble,noise),test_pred)
